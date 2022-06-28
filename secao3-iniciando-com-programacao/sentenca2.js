fcbe98b8e9ecc386aa4a6b73c24fafb013850ab5
Danilo <danilolima180@gmail.com>
2022-06-27 22:45:22 -0300

    fim da aula de sentenca2

---
+++ secao3-iniciando-com-programacao/sentenca2.js
@@ -0,0 +1,6 @@
+console.log(10 * 6);
+console.log("para imprimir textos na tela use entre aspas ou aspas simples JS");
+console.log("Passo #1");
+console.log("Passo #2");
+console.log("Passo #3");
+console.log("Passo #4");
