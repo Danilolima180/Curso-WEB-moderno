e18bb80278baacbbf2fac908704d14ef0b84aac5
Danilo <danilolima180@gmail.com>
2022-06-27 22:22:26 -0300

    Fim da aula1

--- secao3-iniciando-com-programacao/PrimeiroJS.js
+++ secao3-iniciando-com-programacao/PrimeiroJS.js
@@ -1,2 +1,3 @@
-console.log("legal!!");//sentença
-console.log('oi'); // sentença
+console.log("Bom Dia!"); //sentença é um comando dado ao computador para ele executar algo
+console.log("Boa Tarde!"); // sentença
+console.log("Boa Noite!"); //comando executados de cima para baixo
