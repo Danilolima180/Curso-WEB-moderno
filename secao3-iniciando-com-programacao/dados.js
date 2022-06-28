61a77a5befd6190a59875d39eddd0e6307eae114
Danilo <danilolima180@gmail.com>
2022-06-27 23:47:52 -0300

    fim da aula de dados1

---
+++ secao3-iniciando-com-programacao/dados.js
@@ -0,0 +1,21 @@
+console.log("Aula de Dados");
+//console.log("caneta");
+//console.log(10); //valor literal pois não esta explicando da onde vem esse número
+//console.log(6.4); //valor literal
+//console.log(7.9); // valor literal
+
+{
+  let quantidade = 10; // let é a forma atual de declarar a variável
+  let preco = 6.4;
+  let imposto = 1.5;
+  let precoFinal = preco + imposto;
+  let nome = "caneta";
+
+  console.log(nome);
+  console.log(quantidade);
+  console.log(preco);
+  console.log(imposto);
+  console.log(precoFinal);
+  nome = "caneta bic";
+  console.log(nome);
+}
