e6c6eef0647f3e201845c946ef7887ff8501ffee
Danilo <danilolima180@gmail.com>
2022-06-27 23:22:10 -0300

    desafio de bloco e sentenca finalizado

---
+++ secao3-iniciando-com-programacao/desafio_sentenca_bloco.js
@@ -0,0 +1,17 @@
+/* Exibir no console os numeros pares e os numeros impares de 1 a 10 em blocos separados*/
+{
+  console.log("Numeros impares");
+  console.log(1);
+  console.log(3);
+  console.log(5);
+  console.log(7);
+  console.log(9);
+} //fim do bloco de números impares
+{
+  console.log("números pares");
+  console.log(2);
+  console.log(4);
+  console.log(6);
+  console.log(8);
+  console.log(10);
+} // fim dos números pares
