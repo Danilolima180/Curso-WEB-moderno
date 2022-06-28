5785357f1dd44fa5830151271f814891aa0eaf41
Danilo <danilolima180@gmail.com>
2022-06-27 23:14:28 -0300

    fim da aula de bloco 1

---
+++ secao3-iniciando-com-programacao/aula_bloco_de_codigo.js
@@ -0,0 +1,21 @@
+console.log("Aula de Bloco de Código"); //exemplos iniciais sem muito propósito
+{{{
+
+}}}//isto é um bloco de codigo, bloco dentro de bloco, deve haver abertura e fechamento.
+{
+  console.log("passo 1")
+  console.log("passo 2")
+  console.log("passo 3")
+}
+{{}}
+{
+  {
+    {
+      ;
+      ;
+      ;
+    }
+  }
+}
+
+
