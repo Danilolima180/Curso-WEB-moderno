ae3a7f79cd523729851cadf1b2fe92ba44146928
Danilo <danilolima180@gmail.com>
2022-06-27 22:56:01 -0300

    finalizada aula de comentários em JS

--- secao3-iniciando-com-programacao/aula_comentario.js
+++ secao3-iniciando-com-programacao/aula_comentario.js
@@ -1,1 +1,14 @@
+// O console.lo é usado para exibir algo na tela do computador!
 console.log("aula de comentários");
+/*Sempre comentar códigos para facilitar o entendimento de outro programador ou para voce relembrar alguma logica*/
+/* Este é um comentario 
+de multiplas 
+linhas*/
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ */
