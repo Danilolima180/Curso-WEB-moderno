e3d883dd6552812990a61fe442e7e33bee0fb83d
Danilo <danilolima180@gmail.com>
2022-06-28 22:45:44 -0300

    aula explicou sobre as constantes

---
+++ secao3-iniciando-com-programacao/dados4.js
@@ -0,0 +1,7 @@
+console.log("Aula de Dados #4");
+const a = 3;
+///....
+console.log(a); 
+// a = a + 10; da erro pois nao podemos mudar o valor da constante
+///....Mudança de valor da variavel "a".
+console.log(a);
