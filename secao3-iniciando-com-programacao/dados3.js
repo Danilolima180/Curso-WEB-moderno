3bb4fbf4ea52c37daec67a04bcded5e6175831cf
Danilo <danilolima180@gmail.com>
2022-06-28 22:29:45 -0300

    penultima aula sobre dados basicos em JS

---
+++ secao3-iniciando-com-programacao/dados3.js
@@ -0,0 +1,14 @@
+console.log("Aula de Dados 3");
+console.log('tipos de dados')
+
+let idade = 31;
+console.log(typeof 31);
+
+let salario = 4541.25;
+console.log(typeof salario);
+
+let estaChovendo = true //ou false
+console.log(typeof estaChovendo);
+
+
+console.log(typeof "teste");
